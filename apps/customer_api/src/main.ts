ee28aca3f8733ab37d14e868be2740ea8065643f
MarounS36 <120297598+MarounS36@users.noreply.github.com>
2025-08-10 12:23:17 +0300

    Refactor main.ts to implement global validation pipes and enhance application startup; utilize Render's PORT environment variable for better configuration and logging of application URL.

--- apps/customer_api/src/main.ts
+++ apps/customer_api/src/main.ts
@@ -11,11 +11,22 @@ async function bootstrap() {
     NamnamCustomerApiModule,
     new FastifyAdapter()
   );
+  
   app.useGlobalPipes(new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true
-      }));
-  await app.listen(3002, '0.0.0.0');
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+
+  // Use Render's PORT environment variable
+  const port = 3002;
+  
+  await app.listen({ 
+    port: 3002, 
+    host: '0.0.0.0' 
+  });
+  
+  console.log(`Application is running on port ${port}`);
+  console.log('URL:', await app.getUrl());
 }
 bootstrap();
