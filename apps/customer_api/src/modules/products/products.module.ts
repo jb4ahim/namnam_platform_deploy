800aa6c9a2ebfcc37497fc0661b53a24233d57b4
MarounS36 <120297598+MarounS36@users.noreply.github.com>
2026-02-26 22:44:21 +0200

    feat: add StorageModule to ProductsModule imports

--- apps/customer_api/src/modules/products/products.module.ts
+++ apps/customer_api/src/modules/products/products.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@app/database';
+import { StorageModule } from '@app/storage';
 import { ProductsRepository } from './products.repository';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [DatabaseModule, JwtModule],
+    imports: [DatabaseModule, JwtModule, StorageModule],
     controllers: [ProductsController],
     providers: [ProductsService, ProductsRepository],
     exports: [ProductsService],
